77a985d61a1826320c95536f052da4053bfc8c54
Guilherme <guilhermedev96@gmail.com>
2024-07-26 11:38:21 -0300

    style login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,6 +1,32 @@
+import { Link } from "react-router-dom"
+import { Formulario, LoginContainer } from "./styles"
 
 export const Login = () => {
     return (
-        <div>index</div>
+        <LoginContainer>
+            <Formulario>
+                <h3>Mercadinho <br />do Gui!</h3>
+                <fieldset>
+                    <label htmlFor="email">Email</label>
+                    <input
+                        type="email"
+                        id="email"
+                        placeholder="seuemail@gmail.com"
+                    />
+                </fieldset>
+                <fieldset>
+                    <label htmlFor="email">Senha</label>
+                    <input
+                        type="password"
+                        id="password"
+                        placeholder="*******"
+                    />
+                </fieldset>
+                <div>
+                    <button>Logar</button>
+                </div>
+                <p>Não possui conta? <Link to='/register'>registre-se aqui!!</Link></p>
+            </Formulario>
+        </LoginContainer>
     )
 }
